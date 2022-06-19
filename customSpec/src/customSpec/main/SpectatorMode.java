1b74cfab8c187c52ef90dd552c2194985eac3adb
Pokruk <46229617+Pokruk@users.noreply.github.com>
2022-06-19 20:03:11 +0300

    indent fix

--- customSpec/src/customSpec/main/SpectatorMode.java
+++ customSpec/src/customSpec/main/SpectatorMode.java
@@ -5,44 +5,44 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class SpectatorMode {
-  private boolean enabled = false;
-  
-  private Location location;
-  
-  private GameMode initialGamemode;
-  
-  public Player player;
-  
-  SpectatorMode(Player player) {
-    this.player = player;
-  }
-  
-  public boolean isEnabled() {
-    return this.enabled;
-  }
-  
-  public void disable() throws IllegalStateException {
-    if (!this.enabled)
-      throw new IllegalStateException("Can't disable already disabled mode"); 
-    this.player.teleport(this.location);
-    this.player.setGameMode(this.initialGamemode);
-    this.enabled = false;
-  }
-  
-  public void enable() throws IllegalStateException {
-    if (this.enabled)
-      throw new IllegalStateException("Can't enable already enabled mode"); 
-    this.enabled = true;
-    this.location = this.player.getLocation();
-    this.initialGamemode = this.player.getGameMode();
-    this.player.setGameMode(GameMode.SPECTATOR);
-  }
-  
-  public void toggle() {
-    if (isEnabled()) {
-      disable();
-    } else {
-      enable();
-    } 
-  }
+	private boolean enabled = false;
+
+	private Location location;
+
+	private GameMode initialGamemode;
+
+	public Player player;
+
+	SpectatorMode(Player player) {
+		this.player = player;
+	}
+
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	public void disable() throws IllegalStateException {
+		if (!this.enabled)
+			throw new IllegalStateException("Can't disable already disabled mode");
+		this.player.teleport(this.location);
+		this.player.setGameMode(this.initialGamemode);
+		this.enabled = false;
+	}
+
+	public void enable() throws IllegalStateException {
+		if (this.enabled)
+			throw new IllegalStateException("Can't enable already enabled mode");
+		this.enabled = true;
+		this.location = this.player.getLocation();
+		this.initialGamemode = this.player.getGameMode();
+		this.player.setGameMode(GameMode.SPECTATOR);
+	}
+
+	public void toggle() {
+		if (isEnabled()) {
+			disable();
+		} else {
+			enable();
+		}
+	}
 }
--- customSpec/src/customSpec/main/customSpec.java
+++ customSpec/src/customSpec/main/customSpec.java
@@ -13,72 +13,73 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class customSpec extends JavaPlugin implements CommandExecutor, Listener {
-  boolean customSpecEnabled = true;
-  
-  SpectatorModeManager spectatorsManager = new SpectatorModeManager();
-  
-  public void onEnable() {
-    getCommand("customSpec").setExecutor(this);
-    getServer().getPluginManager().registerEvents(this, (Plugin)this);
-  }
-  
-  public void onDisable() {
-    this.spectatorsManager.disableAll();
-  }
-  
-  @EventHandler
-  public void onPlayerQuitEvent(PlayerQuitEvent e) {
-    Bukkit.getLogger().info("PlayerQUIT");
-    SpectatorMode specMode = this.spectatorsManager.getSpectatorModeFor(e.getPlayer());
-    if (specMode.isEnabled())
-      specMode.disable(); 
-    this.spectatorsManager.remove(e.getPlayer());
-  }
-  
-  @EventHandler
-  public void onPlayerKickEvent(PlayerKickEvent e) {
-    Bukkit.getLogger().info("PlayerQUIT");
-    SpectatorMode specMode = this.spectatorsManager.getSpectatorModeFor(e.getPlayer());
-    if (specMode.isEnabled())
-      specMode.disable(); 
-    this.spectatorsManager.remove(e.getPlayer());
-  }
-  
-  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    if (label.equals("customspec") || label.equals("cs")) {
-      if (!this.customSpecEnabled) {
-        sender.sendMessage("This command is off now");
-        return true;
-      } 
-      if (!(sender instanceof Player)) {
-        sender.sendMessage("This command allowed only for players");
-        return true;
-      } 
-      SpectatorMode spectator = this.spectatorsManager.getSpectatorModeFor((Player)sender);
-      spectator.toggle();
-      return true;
-    } 
-    if (label.equals("customspecoff") || label.equals("csoff")) {
-      if (!sender.hasPermission("op")) {
-        sender.sendMessage("You must have op to use that");
-        return true;
-      } 
-      this.spectatorsManager.disableAll();
-      this.spectatorsManager.getSpectators().forEach(spec -> spec.player.sendMessage("CustomSpec turned off by admin"));
-      this.spectatorsManager.clear();
-      this.customSpecEnabled = false;
-      sender.sendMessage("CustomSpec turned off");
-      return true;
-    } 
-    if (label.equals("customspecon") || label.equals("cson")) {
-      if (!sender.hasPermission("op")) {
-        sender.sendMessage("You must have op to use that");
-        return true;
-      } 
-      this.customSpecEnabled = true;
-      sender.sendMessage("CustomSpec turned on");
-      return true;
-    } 
-    return false;
-  }
+	boolean customSpecEnabled = true;
+
+	SpectatorModeManager spectatorsManager = new SpectatorModeManager();
+
+	public void onEnable() {
+		getCommand("customSpec").setExecutor(this);
+		getServer().getPluginManager().registerEvents(this, (Plugin) this);
+	}
+
+	public void onDisable() {
+		this.spectatorsManager.disableAll();
+	}
+
+	@EventHandler
+	public void onPlayerQuitEvent(PlayerQuitEvent e) {
+		Bukkit.getLogger().info("PlayerQUIT");
+		SpectatorMode specMode = this.spectatorsManager.getSpectatorModeFor(e.getPlayer());
+		if (specMode.isEnabled())
+			specMode.disable();
+		this.spectatorsManager.remove(e.getPlayer());
+	}
+
+	@EventHandler
+	public void onPlayerKickEvent(PlayerKickEvent e) {
+		Bukkit.getLogger().info("PlayerQUIT");
+		SpectatorMode specMode = this.spectatorsManager.getSpectatorModeFor(e.getPlayer());
+		if (specMode.isEnabled())
+			specMode.disable();
+		this.spectatorsManager.remove(e.getPlayer());
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equals("customspec") || label.equals("cs")) {
+			if (!this.customSpecEnabled) {
+				sender.sendMessage("This command is off now");
+				return true;
+			}
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command allowed only for players");
+				return true;
+			}
+			SpectatorMode spectator = this.spectatorsManager.getSpectatorModeFor((Player) sender);
+			spectator.toggle();
+			return true;
+		}
+		if (label.equals("customspecoff") || label.equals("csoff")) {
+			if (!sender.hasPermission("op")) {
+				sender.sendMessage("You must have op to use that");
+				return true;
+			}
+			this.spectatorsManager.disableAll();
+			this.spectatorsManager.getSpectators()
+					.forEach(spec -> spec.player.sendMessage("CustomSpec turned off by admin"));
+			this.spectatorsManager.clear();
+			this.customSpecEnabled = false;
+			sender.sendMessage("CustomSpec turned off");
+			return true;
+		}
+		if (label.equals("customspecon") || label.equals("cson")) {
+			if (!sender.hasPermission("op")) {
+				sender.sendMessage("You must have op to use that");
+				return true;
+			}
+			this.customSpecEnabled = true;
+			sender.sendMessage("CustomSpec turned on");
+			return true;
+		}
+		return false;
+	}
 }
